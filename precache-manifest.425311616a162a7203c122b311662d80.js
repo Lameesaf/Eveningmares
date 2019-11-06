fd31557f0a877775c0cfce9c36ccfd1fd2316e9e
Lamees AlFallaj <lafallaj2@gmail.com>
2019-11-07 01:03:53 +0300

    Updates

---
+++ precache-manifest.425311616a162a7203c122b311662d80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d1af7354e2b02c469f2e4cccc1634d",
+    "url": "/Eveningmares/static/media/bg.f2d1af73.gif"
+  },
+  {
+    "revision": "c89f32347d553b16e1eb",
+    "url": "/Eveningmares/static/js/runtime~main.6986ca9b.js"
+  },
+  {
+    "revision": "bdac5568b41c182bf83c",
+    "url": "/Eveningmares/static/js/main.34ee0095.chunk.js"
+  },
+  {
+    "revision": "cebb6b955d75ccb5ac94",
+    "url": "/Eveningmares/static/js/2.aa89cdd4.chunk.js"
+  },
+  {
+    "revision": "bdac5568b41c182bf83c",
+    "url": "/Eveningmares/static/css/main.28cbedbb.chunk.css"
+  },
+  {
+    "revision": "cebb6b955d75ccb5ac94",
+    "url": "/Eveningmares/static/css/2.d3d13ea9.chunk.css"
+  },
+  {
+    "revision": "7fe5bcad9c17796f5ffdd22a6e698bc4",
+    "url": "/Eveningmares/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Eveningmares/precache-manifest.5ff28cce392046d03f94028e6e394a30.js"
+  "/Eveningmares/precache-manifest.425311616a162a7203c122b311662d80.js"
 );
 
 workbox.clientsClaim();
