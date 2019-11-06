90499bc381a6d6c95ecbb079f98c326367cf895b
Lamees AlFallaj <lafallaj2@gmail.com>
2019-11-06 16:56:11 +0300

    Updates

---
+++ precache-manifest.b600c87f8729a113b2c55a1883687dd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c89f32347d553b16e1eb",
+    "url": "/Eveningmares/static/js/runtime~main.6986ca9b.js"
+  },
+  {
+    "revision": "90c5c629f6e82bf361dc",
+    "url": "/Eveningmares/static/js/main.4c41066f.chunk.js"
+  },
+  {
+    "revision": "8538fd8493eaae34fdf9",
+    "url": "/Eveningmares/static/js/2.5c3bab52.chunk.js"
+  },
+  {
+    "revision": "90c5c629f6e82bf361dc",
+    "url": "/Eveningmares/static/css/main.0c8c03e9.chunk.css"
+  },
+  {
+    "revision": "8538fd8493eaae34fdf9",
+    "url": "/Eveningmares/static/css/2.0d4c30b7.chunk.css"
+  },
+  {
+    "revision": "061b1b8d78d9d2d049f6a49a33573695",
+    "url": "/Eveningmares/index.html"
+  }
+];
