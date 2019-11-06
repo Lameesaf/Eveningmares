c6b60fe667a91e9539cea1babe9c7b4f7ec979b3
Lamees AlFallaj <lafallaj2@gmail.com>
2019-11-07 01:02:06 +0300

    add the watch later function

--- src/Component/Jikan/PrintResult.js
+++ src/Component/Jikan/PrintResult.js
@@ -3,21 +3,18 @@ import PrintAnime from './PrintAnime'
 import PrintManga from './PrintManga'
 import PrintCharacter from './PrintCharacter'
 import CardDeck from 'react-bootstrap/CardDeck'
-import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import { Layout } from 'antd';
-const { Content } = Layout;
 
 export default function printResult(props) {
 
   const results = props.results.map((result, index) => {
     switch (props.type) {
       case 'anime':
-        return <PrintAnime length={props.results.length} searchWay={props.searchWay} result={result} key={index} />
+        return <PrintAnime isThere={props.watchLater.includes(result)} toggleWatchLater={(e)=>props.toggleWatchLater(result,props.searchWay,props.type)} length={props.results.length} searchWay={props.searchWay} result={result} key={index} />
       case 'manga':
-        return <PrintManga length={props.results.length} searchWay={props.searchWay} result={result} key={index} />
+        return <PrintManga isThere={props.watchLater.includes(result)} toggleWatchLater={(e)=>props.toggleWatchLater(result,props.searchWay,props.type)} length={props.results.length} searchWay={props.searchWay} result={result} key={index} />
       case 'character':
-        return <PrintCharacter length={props.results.length} result={result} key={index} />
+        return <PrintCharacter  length={props.results.length} result={result} key={index} />
     }
   })
   return (
